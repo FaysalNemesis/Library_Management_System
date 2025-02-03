07bb77bf67e5a404c8b80755c4350a826e10496b
FaysalNemesis <faysalahammednstu13@gmail.com>
2025-02-03 18:26:50 +0600

    added index action and index view

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Library_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly LibraryDbContext _context;
+
+        public BooksController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _context.Books.ToListAsync();
+            return View(books);
+        }
+    }
+}
